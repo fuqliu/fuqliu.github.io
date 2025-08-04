78302a4986f2cd2f20dbf5c02d32b4ff9b7ee704
fuqliu <57979653+fuqliu@users.noreply.github.com>
2025-08-04 10:49:35 +0000

    Deploying to gh-pages from @ fuqliu/fuqliu.github.io@eec6dd78b8b711e8cbf8264f1bc3bd6dbacecc92 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -31,7 +31,7 @@ ninja.data = [{
             },},{id: "news-i-participated-in-neuips-24-safe-generative-ai-with-a-poster-presentation",
           title: 'I participated in NeuIPS’24 Safe Generative AI with a poster presentation.',
           description: "",
-          section: "News",},{id: "news-the-first-author-paper-adversarial-vulnerabilities-in-large-language-models-for-time-series-forecasting-is-accepted-in-aistats-2025",
+          section: "News",},{id: "news-the-first-author-paper-adversarial-vulnerabilities-in-large-language-models-for-time-series-forecasting-was-accepted-in-aistats-2025",
           title: 'The first-author paper, “Adversarial Vulnerabilities in Large Language Models for Time Series Forecasting”,...',
           description: "",
           section: "News",},{id: "news-i-am-glad-to-be-chosen-as-the-best-reviewer-award-in-aistats-25-see",
