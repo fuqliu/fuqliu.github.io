f84537651681a6858abe15e7747d6f13d09d6369
fuqliu <57979653+fuqliu@users.noreply.github.com>
2025-08-04 10:08:45 +0000

    Deploying to gh-pages from @ fuqliu/fuqliu.github.io@1b055cc2e6b4ed9f6f771f68ef29d3ed2f21e544 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -28,16 +28,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-chosen-as-the-best-reviewer-award-in-aistats-25-see",
+          title: 'Chosen as the best reviewer award in AISTATS’25. see',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
