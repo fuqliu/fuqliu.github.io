cc61f894ebee7467f0820a94fa2fd26d98e61509
fuqliu <57979653+fuqliu@users.noreply.github.com>
2025-09-08 22:38:06 +0000

    Deploying to gh-pages from @ fuqliu/fuqliu.github.io@ff4beb6730c86dd3fad0e2558582aa629cbfdbc2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -61,8 +61,8 @@ ninja.data = [{
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-more-accurate-forecasting",
-          title: 'More Accurate Forecasting',
+            },},{id: "projects-more-accurate",
+          title: 'More Accurate',
           description: "Two model-agnostic solutions to improve deep learning–based traffic forecasting.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
